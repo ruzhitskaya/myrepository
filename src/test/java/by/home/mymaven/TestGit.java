b2c505267fab0a4a17cd3cf6f1f07c6cc8083b5f
ruzhitskaya <elenaruz1970@yandex.ru>
2022-07-24 21:54:52 +0300

    Git Test File push and no problem

--- src/test/java/by/home/mymaven/TestGit.java
+++ src/test/java/by/home/mymaven/TestGit.java
@@ -3,5 +3,6 @@ package by.home.mymaven;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("No problem");
     }
 }
