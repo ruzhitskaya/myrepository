00d94b85088e4526182acb17dca9655f3d0b1fdb
ruzhitskaya <elenaruz1970@yandex.ru>
2022-07-24 15:17:35 +0300

    Initial commit

---
+++ src/test/java/by/home/mymaven/Test.java
@@ -0,0 +1,13 @@
+package by.home.mymaven;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven");
+        System.out.println("Hello github");
+        String s = RandomStringUtils.randomAlphabetic(10);
+        System.out.println(s);
+    }
+
+}
