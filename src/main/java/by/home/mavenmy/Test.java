710117272b9cb686986e11f24cb72fce02b971b3
ruzhitskaya <elenaruz1970@yandex.ru>
2022-07-24 22:37:48 +0300

    Test File commit

--- src/main/java/by/home/mavenmy/Test.java
+++ src/main/java/by/home/mavenmy/Test.java
@@ -1,4 +1,4 @@
-package by.home.mymaven;
+package by.home.mavenmy.mymaven;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
--- src/main/java/by/home/mavenmy/TestGit.java
+++ src/main/java/by/home/mavenmy/TestGit.java
@@ -1,4 +1,4 @@
-package by.home.mymaven;
+package by.home.mavenmy.mymaven;
 
 public class TestGit {
     public static void main(String[] args) {
